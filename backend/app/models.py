dfa5f68343bf871e9733860ba31a9b5b6c865dd8
Rian-Ismael <rian.melo@ccc.ufcg.edu.br>
2025-05-16 23:45:25 -0300

    refactor 2: push down

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,10 +7,10 @@ from sqlmodel import Field, Relationship, SQLModel
 # Shared properties
 class EUserBase:
     full_name: str | None = Field(default=None, max_length=255)
-    email: EmailStr = Field(unique=True, index=True, max_length=255)
 
 
 class UserBase(SQLModel, EUserBase):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
     is_active: bool = True
     is_superuser: bool = False
 
